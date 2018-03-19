11bdafa69f366e19e6b18f5935d53dc75b96f834
Jaimy Magalhaes Coelho <mokin009x@hotmail.nl>
2018-03-19 14:34:21 +0100

    First commit

---
+++ Assets/menuScipt.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class menuScipt : MonoBehaviour
+{
+    public GameObject Menu;
+    public GameObject GF;
+    public InputField GuessField;
+
+    private bool ja;
+	// Use this for initialization
+	void Start ()
+	{
+	    ja = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (GuessField.text =="Jermery Clarkson")
+	    {
+	        Application.Quit();
+            Debug.Log("ehi\\kjdskjfjklf");
+             
+	    }
+
+	  
+	}
+
+    public void MenuButton()
+    {
+        if (ja == false)
+        {
+            Menu.SetActive(true);
+            ja = true;
+        }
+        else
+        {
+            Menu.SetActive(false);
+            ja = false;
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Guess()
+    {
+        Menu.SetActive(false);
+        ja = false;
+        GF.SetActive(true);
+
+    }
+}
